dc4b56d2932b1885d932a13dd762c47c7d1cc0a0
Sen <hydrostatic5re@gmail.com>
2022-12-22 13:39:52 +0700

    useRef return type clashes with ref prop type #35572

--- src/component/event/single-event.tsx
+++ src/component/event/single-event.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 function SingleEvent({ data }: any) {
-  const inputEmail = useRef<HTMLInputElement>()
+  const inputEmail = useRef<HTMLInputElement>() as React.MutableRefObject<HTMLInputElement>
   const router = useRouter()
   const [message, setMessage] = useState('')
   //   console.log(router)
